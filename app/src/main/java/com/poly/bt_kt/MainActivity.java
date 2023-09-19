4bd7cd846167c604ea31a5d0e2279a0d8439eafe
Lemited <Leminhtam160223@gmail.com>
2023-09-19 12:01:43 +0700

    bai tap

---
+++ app/src/main/java/com/poly/bt_kt/MainActivity.java
@@ -0,0 +1,14 @@
+package com.poly.bt_kt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
